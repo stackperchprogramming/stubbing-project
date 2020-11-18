4fc4e3758639eea0f57e836aca1b2ebdb87e1679
stackperchprogramming <70243469+stackperchprogramming@users.noreply.github.com>
2020-11-17 19:56:34 -0600

    Update User.h

--- User.h
+++ User.h
@@ -1,12 +1,11 @@
 #pragma once
 using namespace std;
-#include<string> 
+#include <string> 
 #include <time.h>   
 #include <sstream>
 #include <iostream>
-#include <random>
-#include <time.h>
-
+#include <ctime>
+#include <vector>
 
 /***********************************************
 * @Author Ralph Harris
@@ -33,44 +32,118 @@ private:
 		lastName,
 		address,
 		email,
+		uniqueId,
+		password,
+		phoneNumber,
 		error = "Error: This function has not yet been implemented.";
-
-
 public:
 	//initialize to -1 to indicate unset
-	int phoneNumber = -1,
-		uniqueId = -1;
+	int role = 1;// 1 = employee, 2 = manager
+	double seconds = 0;
+	clock_t clockin, clockout;
 	User() {};//default constructor
 
 	//usual getters and setters for this class - self explanatory
-	string getFirstName() { cout << printError(); }
-	void setFirstName(string firstName) { cout << printError(); }
+	string getFirstName() { return firstName; }
+	void setFirstName(string firstName) { this->firstName = firstName; }
 
-	string getLastName() { cout << printError(); }
-	void setLastName(string lastName) { cout << printError(); }
+	string getLastName() { return lastName; }
+	void setLastName(string lastName) { this->lastName = lastName; }
 
-	string getAddress() { cout << printError(); }
-	void setAddress(string address) { cout << printError(); }
+	string getAddress() { return address; }
+	void setAddress(string address) { this->address = address; }
 
-	string getEmail() { cout << printError(); }
-	void setEmail(string email) { cout << printError(); }
+	string getEmail() { return email; }
+	void setEmail(string email) { this->email = email; }
 
-	int getUId() { return uniqueId; }
-	void setUid(int UId) { this->uniqueId = UId; }
+	int getRole() { return role; }
+	void setRole(int role) { this->role = role; }
 
-	int getPhoneNumber() { cout << printError(); }
-	void setPhoneNumber(string PhoneNumber) { cout << printError(); }
+	string getUId() { return uniqueId; }
+	void setUid(string uniqueId) { this->uniqueId = uniqueId; }
+
+	string getPhoneNumber() { return phoneNumber; }
+	void setPhoneNumber(string PhoneNumber) { this->phoneNumber = phoneNumber; }
+
+	string getPassword() { return password; }
+	void setPassword(string password) { this->password = password; }
+
+	int getClockIn() {
+		return clockin / CLOCKS_PER_SEC;
+	}
+	void in() {
+		clockin = clock();
+	}
+	void out() {
+		clockout = clock();
+		incSeconds((clockout - clockin) / CLOCKS_PER_SEC);
+
+		std::cout << "You worked " << (clockout - clockin) / CLOCKS_PER_SEC << "[s]" << std::endl;
+	}
+
+	float getSeconds() {
+		return seconds;
+	}
+	void incSeconds(long seconds) {
+		this->seconds += seconds;
+	}
+	void incHoursinSeconds(double hours) {
+		this->seconds += hours * 3600;
+	}
 
 	string printError() { return error; }
 	//@return String should return a formatted string representation of the user's data
-	string print() { return error; }
-
+	string print() {
+		float hours = seconds / 3600;
+		string info = "[";
+		info += "uniqueId:" + getUId();
+		info += ",firstName:" + getFirstName();
+		info += ",lastName:" + getLastName();
+		info += ",email:" + getEmail();
+		info += ",address:" + getAddress();
+		info += ",phoneNumber:" + (getPhoneNumber());
+		info += ",role:" + intToString(getRole());
+		info += ",hours:" + doubleToString(hours);
+		info += ",password:" + getPassword();
+		info += "]";
+		return info;
+	}
+	string printPretty() {
+		string info = "";
+		info += "uniqueId: " + getUId();
+		info += "\nFirstName: " + getFirstName();
+		info += "\nlastName: " + getLastName();
+		info += "\nemail: " + getEmail();
+		info += "\naddress: " + getAddress();
+		info += "\nphoneNumber: " + (getPhoneNumber());
+		info += "\nrole: " + intToString(getRole());
+		info += "\npassword: " + getPassword();
+		info += "\n";
+		return info;
+	}
+	bool isEmpty() {
+		if (uniqueId.length() < 1)return true;
+		else return false;
+	}
 	void setError(string error) { this->error = error; }
 	string intToString(int integer) {
 		if (integer != -1) {
 			string str;
 			stringstream ss;
 			ss << integer;
+			ss >> str;
+			return str;
+		}
+		else {
+			return error;
+		}
+	}
+	string doubleToString(double num) {
+		if (num != -1) {
+			string str;
+			stringstream ss;
+			ss.precision(15);
+			ss << num;
 			ss >> str;
 			return str;
 		}
